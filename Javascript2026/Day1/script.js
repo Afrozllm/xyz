83581771abdd9ecad4a867280b5d6897b51d0aae
mdafroz3957@gmail.com <mdafrozalam109@gmail.com>
2026-01-23 02:47:24 +0530

    basic javascript :Variable and Data Types

---
+++ Javascript2026/Day1/script.js
@@ -0,0 +1,50 @@
+// Variable hote hain data store karne ke liye//
+// Variable and Data types in javascript//
+// let name="Johan Robert";
+// let age=30;
+// let isStudent=true;
+
+// 
+// let age=20;// kal ko jake Age ki value ko change karna hain to ham let se variable baana ke unko change kar sakte hain//
+// age=30;
+
+// let a=20;
+// console.log(a);//20
+// a=25;
+// console.log(a);//25
+// const age=20;//fixed value
+// age=40; 
+// console.log(age);//error//
+// const pi=3.14;
+// DataTypes//
+// let fname="xyz"//ye hai string data type/////////
+// let age= 23 ye jo hain number data types//
+// let isLogin=true// ye hain boolean data types//
+// let sallary;//undefined data dypes/
+// let address=null;//null data types//
+// console.log(typeof fname);//string//
+// Practices//
+// 📝 Practice Tasks:
+// •
+// Apna naam, age, city variables mein store karo
+// •
+// 5 different data types ke examples banao
+// •
+// typeof operator use karke data type check karo
+//  let My_Name="MD Afroz Alam";//string
+//  let My_age=24;//number//
+//  let City="patna";//string
+
+//  let abx="10";//string//
+//  let ishuman=true//boolean//
+//  let num=100;//number//
+//  let sallary;//undefine//
+// let GasCylender=null;
+// console.log(typeof My_Name)
+// console.log(typeof My_age)
+// console.log(typeof City)
+// console.log(typeof abx)
+// console.log(typeof ishuman)
+// console.log(typeof num)
+// console.log(typeof sallary)
+// console.log(typeof GasCylender)
